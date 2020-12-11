58aaa1b797673b20214e8969ecc885d80cb40e1c
pacomarm <francisco.marmolejoml@udlap.mx>
2020-12-10 23:54:04 -0600

    Added graphs

--- euler.py
+++ euler.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = []
+w = []
+
 def f(t,y):
     return y - (t**2) + 1
 
@@ -6,11 +11,18 @@ def t(h,i):
 
 def euler( N, a, b, alpha):
     h = (b - a)/N
-    w = []
     for i in range(N+1):
+        x.append( a + t(h,i))
         if i == 0:
             w.append(alpha)
         else:
             next = w[i-1] + h*f( t(h,i-1), w[i-1])
             w.append(next)
     return w
+
+euler(10, 0.0, 2.0, .5)
+
+plt.plot(x, w,'red', label='funcion')
+plt.title('Euler simple')
+plt.legend()
+plt.show()
--- eulerMod.py
+++ eulerMod.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = []
+w = []
+
 def f(t,y):
     return y - (t**2) + 1
 
@@ -5,14 +10,20 @@ def t(h,i):
     return h*i
 
 def eulerMod(N, a, b,alpha):
-    w = []
     h = (b-a)/N
     for i in range(11):
+        x.append( a + t(h,i))
         if(i==0):
             w.append(alpha)
         else: 
             temp = w[i-1] + (h)/2 * ( f(t(h,i-1), w[i-1]) + f( t(h,i), w[i-1] + h*f(t(h,i-1), w[i-1])))
             w.append(temp) 
     return w
-    
+
+eulerMod(10, 0.0, 2.0, .5)
+
+plt.plot(x, w,'red', label='funcion')
+plt.title('Euler Mod')
+plt.legend()
+plt.show()   
 
